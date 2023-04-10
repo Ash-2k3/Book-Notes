a8153633c631cb11ad310a4a33b7960ae36f7803
Ash-2k3 <ash.me.cse@gmail.com>
2023-04-10 17:06:55 +0530

    MVC architecture

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from app import controllers
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # set up configuration for the app here
+    app.config['DEBUG'] = True
+    app.register_blueprint(controllers.index_bp)
+
+    return app
