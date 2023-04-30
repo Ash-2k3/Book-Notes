cec4e016e72a0e255ea5e828462c4ba56c19a833
Ash-2k3 <ash.me.cse@gmail.com>
2023-04-30 23:07:54 +0530

    Add function and populating fixed

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,23 +2,23 @@
 import { Book } from "./model.js"
 
 // Global Variables
-const book_lists = [];
 const listOfBooks = document.querySelector('.books-list')
 
 // Functions
 function fetchBooksList() {
-  console.log("Fetching from backend");
+  // console.log("Fetching from backend");
   return new Promise((resolve, reject) => {
     fetch('/get-books')
       .then(response => response.json())
       .then(data => {
+        const book_lists = [];
         for (const book of data) {
-          console.log(book.book_name);
+          // console.log(book.book_name);
           let newBook = new Book(book.book_name);
           book_lists.push(newBook);
         }
-        console.log(book_lists)
-        resolve(data);
+        // console.log(book_lists)
+        resolve(book_lists);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -49,61 +49,63 @@ function createBookInstance(bookName) {
   });
 }
 
-function addBook() {
+async function addBook() {
   const bookNameTextField = document.querySelector('.book-input-form');
   const bookName = bookNameTextField.value;
-  const newListElement = document.createElement("li");
-  newListElement.textContent = bookName
-  console.log(newListElement);
-  listOfBooks.append(newListElement);
-  createBookInstance(bookName);
+  await createBookInstance(bookName);
+  await populateUI();
+  // console.log(here);
 }
 
 async function populateUI() {
-  await fetchBooksList()
+  listOfBooks.innerHTML = "";
+  let book_lists = await fetchBooksList();
+  console.log(listOfBooks);
+  let i = 0;
   for (const book of book_lists) {
-         const card = document.createElement("div");
-         card.classList.add("card", "mb-3");
- 
-         const row = document.createElement("div");
-         row.classList.add("row", "g-0");
- 
-         const colImg = document.createElement("div");
-         colImg.classList.add("col-md-4");
- 
-         const img = document.createElement("img");
-         img.classList.add("img-fluid", "rounded-start");
-         // img.setAttribute("src", book.image_url);
-         // img.setAttribute("alt", book.title);
- 
-         const colBody = document.createElement("div");
-         colBody.classList.add("col-md-8");
- 
-         const cardBody = document.createElement("div");
-         cardBody.classList.add("card-body");
- 
-         const title = document.createElement("h5");
-         title.classList.add("card-title");
-         title.textContent = book.bookName;
- 
-         const description = document.createElement("p");
-         description.classList.add("card-text");
-         description.textContent = book.bookName;
- 
-         const timestamp = document.createElement("p");
-         timestamp.classList.add("card-text");
-         const timestampText = document.createElement("small");
-         timestampText.classList.add("text-muted");
-         timestampText.textContent = `Last updated ago`;
-         timestamp.append(timestampText);
- 
-         colImg.append(img);
-         colBody.append(cardBody);
-         cardBody.append(title, description, timestamp);
-         row.append(colImg, colBody);
-         card.append(row);
- 
-         listOfBooks.append(card);
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-3");
+
+    const row = document.createElement("div");
+    row.classList.add("row", "g-0");
+
+    const colImg = document.createElement("div");
+    colImg.classList.add("col-md-4");
+
+    const img = document.createElement("img");
+    img.classList.add("img-fluid", "rounded-start");
+    // img.setAttribute("src", book.image_url);
+    // img.setAttribute("alt", book.title);
+
+    const colBody = document.createElement("div");
+    colBody.classList.add("col-md-8");
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = book.book_name;
+    console.log(book.book_name);
+
+    const description = document.createElement("p");
+    description.classList.add("card-text");
+    description.textContent = book.book_name;
+
+    const timestamp = document.createElement("p");
+    timestamp.classList.add("card-text");
+    const timestampText = document.createElement("small");
+    timestampText.classList.add("text-muted");
+    timestampText.textContent = `Last updated ${i} ago`;
+    timestamp.append(timestampText);
+    i++;
+    colImg.append(img);
+    colBody.append(cardBody);
+    cardBody.append(title, description, timestamp);
+    row.append(colImg, colBody);
+    card.append(row);
+
+    listOfBooks.append(card);
   }
 }
 
